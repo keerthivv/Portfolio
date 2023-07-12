76944b80b6fdefe743938ab2ef3282dc387f7c56
keerthivv <40594850+keerthivv@users.noreply.github.com>
2023-07-12 16:36:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirectWordcraft() {
+    window.location.href = "https://647f1b8b5e1d3516f9bac5a2--merry-cascaron-759c5b.netlify.app/";
+  }
+
+  function redirectdental() {
+    window.location.href = "https://github.com/keerthivv/Dental";
+  }
+
+function redirectportfolio(){
+  window.location.href = "https://github.com/keerthivv/Portfolio";
+}
